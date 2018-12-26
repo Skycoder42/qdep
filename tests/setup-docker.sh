8b231a4234e3f56f02191e78ee86097677b39b0b
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-12-26 17:41:07 +0100

    ..

--- tests/setup-docker.sh
+++ tests/setup-docker.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-	PIP=pip3
-else
-	PIP=pip
-fi
-
-$SUDO $PIP install appdirs lockfile
+$SUDO pip3 install appdirs lockfile
 $SUDO ./qdep.py prfgen --qmake "/opt/qt/$QT_VER/$PLATFORM/bin/qmake"
 mv tests/qdep.pro ./
