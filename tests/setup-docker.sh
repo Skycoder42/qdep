6e7dc28b8eb259d2a4b15f044ddf1e8d7e6e893d
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-12-26 15:15:39 +0100

    ..

--- tests/setup-docker.sh
+++ tests/setup-docker.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-SCRIPT_PATH="$(readlink -f "$(dirname "$0")")"
-
 ./qdep.py prfgen --qmake "/opt/qt/$QT_VER/$PLATFORM/bin/qmake"
-
 mv tests/qdep.pro ./
--- tests/setup-travis.sh
+++ tests/setup-travis.sh
@@ -6,7 +6,7 @@ currDir=$(dirname $0)
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then	
 	# append post build script
 	mv qtmodules-travis/ci/linux/build-docker.sh qtmodules-travis/ci/linux/build-docker.sh.bkp
-	echo "$currDir/setup-docker.sh" > qtmodules-travis/ci/linux/build-docker.sh
+	cat "$currDir/setup-docker.sh" > qtmodules-travis/ci/linux/build-docker.sh
 	cat qtmodules-travis/ci/linux/build-docker.sh.bkp >> qtmodules-travis/ci/linux/build-docker.sh
 	rm qtmodules-travis/ci/linux/build-docker.sh.bkp
 	chmod a+x qtmodules-travis/ci/linux/build-docker.sh
