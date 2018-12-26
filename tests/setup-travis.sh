162ed4f2aec358bf8aef42ad3122cbb565ea33e6
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-12-26 14:07:22 +0100

    ....

--- tests/setup-travis.sh
+++ tests/setup-travis.sh
@@ -9,6 +9,7 @@ if [[ $TRAVIS_OS_NAME == "linux" ]]; then
 	echo "$currDir/setup-docker.sh" > qtmodules-travis/ci/linux/build-docker.sh
 	cat qtmodules-travis/ci/linux/build-docker.sh.bkp >> qtmodules-travis/ci/linux/build-docker.sh
 	rm qtmodules-travis/ci/linux/build-docker.sh.bkp
+	chmod a+x qtmodules-travis/ci/linux/build-docker.sh
 else
 	$currDir/setup-docker.sh
 fi
