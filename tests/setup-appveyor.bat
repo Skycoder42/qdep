07a4f54003ea5dfa19dd5eb1a795f336ce08a068
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-12-26 17:52:06 +0100

    ....

--- tests/setup-appveyor.bat
+++ tests/setup-appveyor.bat
@@ -1,5 +1,5 @@
 @echo off
 
-C:\Python37-x64\python.exe -c "pip install appdirs lockfile" || exit /B 1
+C:\Python37-x64\Scripts\pip.exe install appdirs lockfile || exit /B 1
 C:\Python37-x64\python.exe ./qdep.py prfgen --qmake "C:\projects\Qt\%QT_VER%\%PLATFORM%\bin\qmake" || exit /B 1
 move tests\qdep.pro .\dep.pro || exit /B 1
