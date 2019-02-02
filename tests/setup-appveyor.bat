f481a11a7db7fe6e1102b177e7c1a8bfcd5aa1b4
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-02-02 13:08:39 +0100

    ...

--- tests/setup-appveyor.bat
+++ tests/setup-appveyor.bat
@@ -1,9 +1,12 @@
 @echo off
 
+:: install dependencies
 C:\Python37-x64\Scripts\pip.exe install appdirs lockfile argcomplete setuptools || exit /B 1
-move tests\qdep.pro .\dep.pro || exit /B 1
 C:\Python37-x64\Scripts\pip.exe uninstall -y qdep || exit /B 1
 
-:: install qdep module
+:: install/prepare qdep
 C:\Python37-x64\Scripts\pip.exe install -e . || exit /B 1
 C:\Python37-x64\Scripts\qdep.exe prfgen --qmake "C:\projects\Qt\%QT_VER%\%PLATFORM%\bin\qmake" || exit /B 1
+
+:: move project file
+move tests\qdep.pro .\dep.pro || exit /B 1
--- tests/setup-docker.sh
+++ tests/setup-docker.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+# install dependencies
 $SUDO pip3 install appdirs lockfile argcomplete setuptools
 $SUDO pip3 uninstall -y qdep
 
+# install/prepare qdep
 $SUDO pip3 install -e .
 $SUDO qdep prfgen --qmake "/opt/qt/$QT_VER/$PLATFORM/bin/qmake"
+
+# move project file
+mv tests/qdep.pro ./
