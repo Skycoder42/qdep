9856e1b9075b11180b654a0188709bf96b345965
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-02-02 13:03:08 +0100

    uninstall yes

--- tests/setup-docker.sh
+++ tests/setup-docker.sh
@@ -2,9 +2,7 @@
 set -e
 
 $SUDO pip3 install appdirs lockfile argcomplete setuptools
-if pip3 list | grep qdep; then
-	$SUDO pip3 uninstall qdep
-fi
+$SUDO pip3 uninstall -y qdep
 
 $SUDO pip3 install -e .
 $SUDO qdep prfgen --qmake "/opt/qt/$QT_VER/$PLATFORM/bin/qmake"
